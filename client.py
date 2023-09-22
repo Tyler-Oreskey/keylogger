c937078dfbfce6f64523c0d3bcb742a041de62ff
Tyler-Oreskey <toreskey@gmail.com>
2023-09-22 19:47:20 +0000

    commit

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
-# Import socket module
 import socket
 
-# s.close()
 
-# create an INET, STREAMing socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# now connect to the web server on port 80 - the normal http port
-s.connect(("127.0.0.1", 1236))
+def client_program():
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 1236  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    message = input(" -> ")  # take input
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Received from server: ' + data)  # show in terminal
+
+        message = input(" -> ")  # again take input
+
+    client_socket.close()  # close the connection
+
+
+if __name__ == '__main__':
+    client_program()
--- server.py
+++ server.py
@@ -1,8 +1,31 @@
 import socket
 
-# create an INET, STREAMing socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# bind the socket to a public host, and a well-known port
-serversocket.bind((socket.gethostname(), 1236))
-# become a server socket
-serversocket.listen(5)
+
+def server_program():
+    # get the hostname
+    host = socket.gethostname()
+    port = 1236  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many clients the server can listen to simultaneously
+    server_socket.listen(1)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("from connected user: " + str(data))
+        data = input(' -> ')
+        conn.send(data.encode())  # send data to the client
+
+    conn.close()  # close the connection
+
+
+if __name__ == '__main__':
+    server_program()
